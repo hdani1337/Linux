1fc09292a9d7dc5b4e957562468a07d1d5b0f324
Horváth Dániel <h.danika07@gmail.com>
2020-03-05 13:18:01 +0100

    Windowsban elmentett hálózati fiókok törlése

---
+++ mentettFiokokTorlese.bat
@@ -0,0 +1,8 @@
+@echo off
+cmdkey.exe /list > "%TEMP%\List.txt"
+findstr.exe Target "%TEMP%\List.txt" > "%TEMP%\fiokok.txt"
+FOR /F "tokens=1,2 delims= " %%G IN (%TEMP%\fiokok.txt) DO cmdkey.exe /delete:%%H
+del "%TEMP%\List.txt" /f /q
+del "%TEMP%\fiokok.txt" /f /q
+echo Minden elmentett fiok sikeresen torolve
+pause
